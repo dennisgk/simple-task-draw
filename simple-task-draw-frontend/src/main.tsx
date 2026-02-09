4a0fa4cc8c9c1b2fa0757a0e4b1063e2a0386772
Dennis Kountouris <dennisgk2005@gmail.com>
2026-02-08 19:47:10 -0500

    com

---
+++ simple-task-draw-frontend/src/main.tsx
@@ -0,0 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")!).render(<Container fluid />);
