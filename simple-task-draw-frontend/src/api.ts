aa2cf51e4c14af147757798745a0f01f48a95618
Dennis Kountouris <dennisgk2005@gmail.com>
2026-02-08 20:51:05 -0500

    com

--- simple-task-draw-frontend/src/api.ts
+++ simple-task-draw-frontend/src/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE ?? "http://localhost:8000";
+const API_BASE = "https://std.kountouris.org";
 
 async function fetchJson<T>(input: RequestInfo | URL, init?: RequestInit): Promise<T> {
   const response = await fetch(input, {
